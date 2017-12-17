0ae4c353aaa236baf789325911fa7b2fe1be4026
GodFuckABitch <sylvain.combraque@etudiant.univ-reims.fr>
2017-12-17 22:02:50 +0100

    Contact done, send mail ok must add login feature

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,41 +5,58 @@ namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class ContactController extends Controller
 {
     /**
      * @Route("/me-contacter", name="contact")
      */
-    public function index()
+    public function index(\Swift_Mailer $mailer)
     {
 
+        $isMailSent = false;
+        $session = new Session();
+        $session->start();
+        $is_connected = $session->get('isConnected');
+
         if(isset($_POST)){
-            if(isset($_POST['username']) && isset($_POST['userfirstname']) && isset($_POST['email']) && isset($_POST['description'])){
-                $transport = (new Swift_SmtpTransport('smtp.gmail.com', 25))
-                    ->setUsername('compte.asfeld@gmail.com')
-                    ->setPassword('6edb53506f062ec8b774f30fcd6dea3fd376a713')
+            if(isset($_POST['username']) && isset($_POST['userfirstname']) && isset($_POST['usermail']) && isset($_POST['description'])){
+
+                $isMailSent = true;
+
+                $message = (new \Swift_Message('Hello Email'))
+                    ->setFrom('sylvaincombraquemailservice@gmail.com')
+                    ->setTo('sylvaincombraque@hotmail.fr')
+                    ->setBody(
+                        $this->renderView(
+                        // app/Resources/views/Emails/registration.html.twig
+                            'mail.html.twig'
+                        ),
+                        'text/html'
+                    )
+                    /*
+                     * If you also want to include a plaintext version of the message
+                    ->addPart(
+                        $this->renderView(
+                            'Emails/registration.txt.twig',
+                            array('name' => $name)
+                        ),
+                        'text/plain'
+                    )
+                    */
                 ;
 
-// Create the Mailer using your created Transport
-                $mailer = new Swift_Mailer($transport);
-
-// Create a message
-                $message = (new Swift_Message('Wonderful Subject'))
-                    ->setFrom(['compte.asfeld@gmail.com' => 'Compte'])
-                    ->setTo(['sylvaincombraque@hotmail.fr' => 'Sylvain COMBRAQUE'])
-                    ->setBody('Hello test')
-                ;
-
-// Send the message
                 $mailer->send($message);
+
             }
         }
 
         return $this->render('contact.html.twig', array(
             'title' => "Me contacter",
             'body' => "Contactez-moi en remplissant le formulaire suivant",
-            'connected' => true,
+            'connected' => $is_connected,
+            'isMailSent' => $isMailSent,
         ));
     }
 }
---
+++ src/Entity/Person.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PersonRepository")
+ */
+class Person
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $pseudo;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param mixed $firstname
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param mixed $lastname
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPseudo()
+    {
+        return $this->pseudo;
+    }
+
+    /**
+     * @param mixed $pseudo
+     */
+    public function setPseudo($pseudo)
+    {
+        $this->pseudo = $pseudo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+
+
+}
